82ef89f9bc73223ff45cde61ac03fefd5f97e916
heokyunam <heokyunam@naver.com>
2013-12-10 23:06:38 +0900

    12/10 11:05 허규남

--- OOD_BounceBall/src/GameRes/Needle.java
+++ OOD_BounceBall/src/GameRes/Needle.java
@@ -1,13 +1,28 @@
 package GameRes;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Polygon;
 
 public class Needle extends GameObject{
-
+	private final Color c = Color.red;
+	private Polygon p;
+	public Needle() {
+		this(0, 0, 0, 0);
+	}
+	public Needle(int x, int y, int height, int width) {
+		super(x, y, height, width);
+	}
 	@Override
 	public void draw(Graphics g) {
-		// TODO Auto-generated method stub
-		
+		if(p == null) {
+			p = new Polygon();
+			p.addPoint(x, y + height);
+			p.addPoint(x + width/2, y);
+			p.addPoint(x + width, y + height);			
+		}
+		g.setColor(c);
+		g.fillPolygon(p);
 	}
 
 	@Override
